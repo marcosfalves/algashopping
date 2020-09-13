d18e3edccdd81d1420844994ce6d017c18453603
marcosfalves <marcosf_alves@outlook.com>
2020-09-13 00:54:16 -0300

    create LineChart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Checkbox from '../../shared/Checkbox/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 import AppContainer from '../AppContainer'
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
@@ -7,6 +8,15 @@ import { Container, Wrapper } from './App.styles'
 function App() {
     const [lettuce, setLettuce] = useState(true)
     const [rice, setRice] = useState(false)
+
+    const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+
+    /*const [healthy, setHealthy] = useState(20)
+    useEffect(function (){
+        setTimeout(() => {
+            setHealthy(80)
+        }, 5000)
+    },[])*/
 
     return <Wrapper>
         <Container>
@@ -33,6 +43,10 @@ function App() {
             }
             right={<div>
                 estatisticas
+                <LineChart color={colors[0]} title="Saudável" percentage={80}/>
+                <LineChart color={colors[1]}title="Não tão saudável" percentage={20}/>
+                <LineChart color={colors[2]} title="Limpeza" percentage={35}/>
+                <LineChart color={colors[3]} title="Outros" percentage={15}/>
             </div>
             }
             />
